be9a8f7624e01ea4bcca17df208ab1905180c695
ChriEffe <christianferrazzano93@gmail.com>
2021-11-18 14:47:13 +0100

    js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 3.  poi chiedi il suo colore preferito
 4.  Infine scrivi sulla pagina nomecognomecolorepreferito21*/
 
+
+// Richiesta nome utente
+let nomeUtente = prompt('Inserisci il tuo nome');
+console.log('Nome Utente ' + nomeUtente);
+
+// Richiesta cognome utente
+let cognomeUtente = prompt('Inserisci il tuo cognome');
+console.log('Cognome Utente ' + cognomeUtente);
+
+// Richiesta colore preferito
+let colorePreferito = prompt('Inserisci colore preferito');
+console.log('Colore Preferito ' + colorePreferito);
+
+// Scritto su pagina
+document.writeln('Dati personali ' + nomeUtente + cognomeUtente + colorePreferito);
